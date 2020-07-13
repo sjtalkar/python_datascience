acf1631de3df84f2a4303b379f3ff71f8acb722b
sjtalkar <sjtalkar@live.com>
2020-07-12 17:36:59 -0700

    Resample Rename TimeSeries

--- TimeSeriesResampleRename.py
+++ TimeSeriesResampleRename.py
@@ -1,53 +1,31 @@
-def convert_housing_data_to_quarters():
-    """Converts the housing data to quarters and returns it as mean 
-    values in a dataframe. This dataframe should be a dataframe with
-    columns for 2000q1 through 2016q3, and should have a multi-index
-    in the shape of ["State","RegionName"].
-    
-    Note: Quarters are defined in the assignment description, they are
-    not arbitrary three month periods.
-    
-    The resulting dataframe should have 67 columns, and 10,730 rows.
-    """
-
-    return "ANSWER"
+zillow_allhomes_df = pd.read_csv("City_Zhvi_AllHomes.csv")
+zillow_allhomes_df.head()
 
 
-# A quarter is a specific three month period, Q1 is January through March,
-# Q2 is April through June, Q3 is July through September, Q4 is October through December.
-
-len(zillow_allhomes_df)
-zillow_allhomes_df.columns
+zillow_allhomes_df.tail()
 # Index(['RegionID', 'RegionName', 'State', 'Metro', 'CountyName', 'SizeRank', '2016-01', '2016-02', '2016-03', '2016-04',
-temp_zillow = zillow_allhomes_df[zillow_allhomes_df["State"] == "WA"]
+temp_zillow = zillow_allhomes_df.copy()
+#  convert column  names to datetime
+temp_zillow.set_index(["State", "RegionName"], inplace=True)
 
-# print((2016-2000 + 1 ) * 4)
-
-# separate time columns and convert their names to datetime
 temp_zillow = temp_zillow[temp_zillow.columns[6:]].rename(columns=pd.to_datetime)
+# print(temp_zillow.shape)
 
-column_we_need = temp_zillow.columns
-column_we_need = column_we_need[
-    (column_we_need > "1999-12-31") & (column_we_need < "2016-09-01")
+columns_we_need = temp_zillow.columns
+columns_we_need = columns_we_need[
+    (columns_we_need > "1999-12-31") & (columns_we_need < "2016-09-01")
 ]
-len(column_we_need) / 3
 
-temp_zillow[column_we_need]
-
-
-temp_zillow = temp_zillow[column_we_need].resample("QS", axis=1).mean()
-# quarterly_mean
+temp_zillow = temp_zillow[columns_we_need].resample("QS", axis=1).mean()
 
 
+# Convert dates to Quarter periods
 month_names = temp_zillow.columns
 month_names = pd.PeriodIndex(month_names, freq="Q")
 
+# Assign Quater names as column names
 temp_zillow.columns = month_names
-
-# zillow_allhomes_df[zillow_allhomes_df.columns[:6]]
-temp_zillow[temp_zillow.columns[:6]]
-
-
-# print (tabulate(mdf[mdf.columns[0:9]].iloc[
-#        23:29], headers='keys', tablefmt='orgtbl'))
+zillow_allhomes_df = temp_zillow
+zillow_allhomes_df.iloc[18]
+zillow_allhomes_df.loc[("WA", "Seattle")]
 
